8ce898bf6577d4770a7f4c7d3f311d5e27ab52eb
Justin <justinhwu95@gmail.com>
2019-05-20 10:16:50 -0400

    Done.

--- index.js
+++ index.js
@@ -28,11 +28,6 @@ class Bird{
   }
 
   speak(){
-    if (this.sex === 'male'){
-      return `It's me! ${this.name}, the parrot!`
-    }
-    else{
-      return `${this.name} says squawk!`
-    }
+    return (this.sex === 'male') ? `It's me! ${this.name}, the parrot!` : `${this.name} says squawk!`
   }
 }
