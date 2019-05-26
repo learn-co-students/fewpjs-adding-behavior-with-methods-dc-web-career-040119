84bc9678b4577cbe686cea8861c3d2a16f72326f
bmanek <41875460+bmanek@users.noreply.github.com>
2019-05-26 12:08:57 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Your code here
+
+class Animal {
+  constructor(name, sex) {
+    this.name = name
+    this.sex = sex
+  }
+}
+
+class Dog extends Animal {
+  speak() {
+    return `${this.name} says woof!`
+  }
+
+}
+
+class Cat extends Animal {
+  speak() {
+    return `${this.name} says meow!`
+  }
+}
+
+class Bird extends Animal {
+  speak() {
+    if (this.sex === "male") {
+      return `It's me! ${this.name}, the parrot!`
+    } else {
+    return `${this.name} says squawk!`
+    }
+  }
+}
